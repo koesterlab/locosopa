606a2533770a32c3efe2f5cd43a2e828d406000b
Johannes Köster <johannes.koester@uni-due.de>
2024-07-10 14:27:18 +0200

    Update conf.py

--- locosopa/src/docs/conf.py
+++ locosopa/src/docs/conf.py
@@ -21,7 +21,7 @@ date = datetime.now()
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autosectionlabel']
+extensions = ["sphinx.ext.autosectionlabel", "sphinxawesome_theme.highlighting"]
 
 # templates_path = ['_templates']
 exclude_patterns = []
